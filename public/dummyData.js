1d9e55619400cea8edca23f830411e66d0e69584
Carlos Venencia <carlos.venencia@hotmail.com>
2022-11-26 17:16:29 -0500

    Changed user IDs for dummy data

--- public/dummyData.js
+++ public/dummyData.js
@@ -7,25 +7,25 @@ if (!usersStorage) {
     console.log('No user data found. Creating dummy data...');
     const users = [
         {
-            id: 1,
+            id: '6e200475-fe19-4c23-97a8-c5f5ab1a3f58',
             username: 'comprador1',
             password: '123',
             type: 'buyer',
         },
         {
-            id: 2,
+            id: 'bbb693a0-4f1a-46c7-9e2f-ae028a3f6f36',
             username: 'comprador2',
             password: '456',
             type: 'buyer',
         },
         {
-            id: 3,
+            id: '41438153-ed09-405e-8ddf-d48f593c00f1',
             username: 'vendedor1',
             password: 'abc',
             type: 'seller',
         },
         {
-            id: 4,
+            id: '52663413-062c-4e40-853f-b857ed0ed22c',
             username: 'vendedor2',
             password: 'def',
             type: 'buyer',
