88e76a1fad0bc704d59b97029428f5d12dc492f6
Carlos Venencia <carlos.venencia@hotmail.com>
2022-11-30 23:17:05 -0500

    Save user on refresh and fix navigation when user is already logged in

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,16 +1,29 @@
-import { createContext, useState, useEffect } from "react"
+import { createContext, useState, useEffect } from 'react';
+import { setLocalStorage, getLocalStorage } from '../api/util';
 
-const UserContext = createContext()
+const UserContext = createContext();
 
 function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
 
-    const [user, setUser] = useState(null)
+    function logout() {
+        localStorage.removeItem('currentUser');
+        setUser(null);
+    }
+
+    useEffect(() => {
+        user != null && setLocalStorage('currentUser', user);
+    }, [user]);
+
+    useEffect(() => {
+        setUser(getLocalStorage('currentUser') || null);
+    }, []);
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, logout }}>
             {children}
         </UserContext.Provider>
-    )
+    );
 }
 
-export { UserContext, UserProvider }
+export { UserContext, UserProvider };
